8fceab5ea8841b7565be8f79164e219a992b74d1
CulturalMass <melherrra@hotmail.com>
2023-10-13 16:50:09 +0200

    first commit

---
+++ types/index.ts
@@ -0,0 +1,45 @@
+export type PriceHistoryItem = {
+  price: number;
+};
+
+export type User = {
+  email: string;
+};
+
+export type Product = {
+  _id?: string;
+  url: string;
+  currency: string;
+  image: string;
+  title: string;
+  currentPrice: string;
+  originalPrice: string;
+  priceHistory: PriceHistoryItem[] | [];
+  highestPrice: string;
+  lowestPrice: string;
+  averagePrice: string;
+  discountRate: number;
+  description: string;
+  category: string;
+  reviewsCount: number;
+  stars: number;
+  isOutOfStock: Boolean;
+  users?: User[];
+};
+
+export type NotificationType =
+  | "WELCOME"
+  | "CHANGE_OF_STOCK"
+  | "LOWEST_PRICE"
+  | "THRESHOLD_MET";
+
+export type EmailContent = {
+  subject: string;
+  body: string;
+};
+
+export type EmailProductInfo = {
+  title: string;
+  url: string;
+  image?: string;
+};
